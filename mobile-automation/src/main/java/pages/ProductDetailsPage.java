dd22740021fa98ae087923bec915bc3a52687c9c
Jitesh Badgujar <40344180+jitesh-badgujar@users.noreply.github.com>
2023-05-07 20:08:36 +0530

    return recently added product to cart for further validation

--- mobile-automation/src/main/java/pages/ProductDetailsPage.java
+++ mobile-automation/src/main/java/pages/ProductDetailsPage.java
@@ -10,6 +10,7 @@ public class ProductDetailsPage extends BasePage {
 
 	private By addToCartButton = AppUtils.getByResourceId("add-to-cart-button");
 	private By buyBoxView = AppUtils.getByResourceId("buybox");
+	// private By priceText = By.id("");
 
 	public ProductDetailsPage() {
 		super();
@@ -20,6 +21,18 @@ public class ProductDetailsPage extends BasePage {
 		scrollDownTillElementAppears(buyBoxView);
 		scrollDownTillElementAppears(addToCartButton);
 		driver.findElement(addToCartButton).click();
+		/*
+		 * Return currently added product for further validation
+		 * 
+		 * Product currentProduct = new Product(); By priceValue =
+		 * AppUtils.getByResourceId("");
+		 * 
+		 * String price = driver.findElement(priceText).getText();
+		 * 
+		 * currentProduct.setPrice(Float.parseFloat(price));
+		 * 
+		 * return currentProduct;
+		 */
 	}
 
 	public void selectBuyingOptions(Map<String, String> map) {
