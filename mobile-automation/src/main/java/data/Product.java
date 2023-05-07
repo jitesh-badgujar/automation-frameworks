a445f006810fb3fba066d016044e87bbcd799ce3
Jitesh Badgujar <40344180+jitesh-badgujar@users.noreply.github.com>
2023-05-07 20:04:12 +0530

    Add Product class to store details of items added to cart

---
+++ mobile-automation/src/main/java/data/Product.java
@@ -0,0 +1,32 @@
+package data;
+
+public class Product {
+
+	private String name;
+	private float price;
+	private int quantity;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
