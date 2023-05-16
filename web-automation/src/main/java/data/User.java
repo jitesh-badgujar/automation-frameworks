0dff310e77969eb2034cd9bcd869c711964ab814
Jitesh Badgujar <40344180+jitesh-badgujar@users.noreply.github.com>
2023-05-16 17:58:01 +0530

    Update User.java

--- web-automation/src/main/java/data/User.java
+++ web-automation/src/main/java/data/User.java
@@ -18,13 +18,4 @@ public class User {
 	public String getPassword() {
 		return password;
 	}
-
-	public static User getStandardUser() {
-		return new User("standard_user", "secret_sauce");
-	}
-
-	public static User getInvalidUser() {
-		return new User("locked_out_user", "secret_sauce");
-	}
-
 }
